f8f93159e6dd9f26a99175dd12c3b8806cf5f5a8
OPuig23 <oriol23puig@gmail.com>
2025-12-30 00:57:46 +0100

    fix(pwa): add head.tsx and manifest

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,16 +7,19 @@ export default function Head() {
       <meta name="description" content="WebApp operativa de Cal Blay" />
       <meta name="theme-color" content="#1e293b" />
 
-      {/* PWA */}
+      {/* Manifest PWA */}
       <link rel="manifest" href="/manifest.json" />
 
-      {/* Android icons */}
+      {/* Favicons / Android */}
+      <link rel="icon" href="/icons/icon-192.png" />
       <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192.png" />
       <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512.png" />
 
       {/* iOS */}
       <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Cal Blay" />
+      <link rel="apple-touch-icon" href="/icons/icon-192.png" />
     </>
   )
 }
